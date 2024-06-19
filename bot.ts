921897337c0599ae5a86d104a21b86b18d65b166
unknown <particular6@proton.me>
2024-06-20 02:17:25 +0500

    session flavor

--- bot.ts
+++ bot.ts
@@ -1,20 +1,33 @@
-import { Bot } from 'grammy';
+import { Bot, Context, session, SessionFlavor } from 'grammy';
 import * as dotenv from "dotenv";
 
 dotenv.config();
 const token = process.env.TOKEN || '';
-console.log(token);
-if (!token) throw new Error('Something is wrong with the token..');
+const targetChatID = process.env.TARGET_CHAT_ID || '';
 
-const bot = new Bot(token);
+interface SessionData {
+    curStep: number;
+    answers: string[];
+}
+
+type MyContext = Context & SessionFlavor<SessionData>;
+
+const bot = new Bot<MyContext>(token);
+
+const questions = [
+    'Are you a student or a teacher?',
+    'What subject would you like to learn or teach?',
+    'How old are you?'
+]
 
 bot.command('start', (ctx) => {
-    ctx.reply('Hello!! I will try to connect you with a student or a tutor');
+    ctx.reply('Hello!! Here we connect those willing to teach and willing to learn. Type /go to start');
 })
 
 bot.on('message', (ctx) => {
-    console.log('user has sent something...');
-    ctx.reply('You are heard, but not understood yet');
+    // console.log(ctx.chat.id);
+    bot.api.sendMessage(targetChatID, 'WORKS HUH??');
+    // ctx.reply('You are heard, but not understood yet');
 })
 
 bot.start();
