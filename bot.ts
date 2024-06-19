87a25b6c5cbbdb0c42d60c29d0004a578bd2c716
unknown <particular6@proton.me>
2024-06-20 02:57:22 +0500

    questions are now asked with the answers being sent to another chat... implemented in an ugly way for now, though

--- bot.ts
+++ bot.ts
@@ -14,6 +14,13 @@ type MyContext = Context & SessionFlavor<SessionData>;
 
 const bot = new Bot<MyContext>(token);
 
+function initial(): SessionData {
+    return { curStep: 0,
+             answers: []
+            };
+  }
+  bot.use(session({ initial }));
+
 const questions = [
     'Are you a student or a teacher?',
     'What subject would you like to learn or teach?',
@@ -21,13 +28,25 @@ const questions = [
 ]
 
 bot.command('start', (ctx) => {
-    ctx.reply('Hello!! Here we connect those willing to teach and willing to learn. Type /go to start');
+    ctx.reply('Hello!! Here we connect those willing to teach and willing to learn. Tell me, are you a student or a teacher?');
 })
 
 bot.on('message', (ctx) => {
-    // console.log(ctx.chat.id);
-    bot.api.sendMessage(targetChatID, 'WORKS HUH??');
-    // ctx.reply('You are heard, but not understood yet');
+    let curStep = ctx.session.curStep;
+    if (curStep < questions.length) {
+        ctx.session.answers.push(ctx.message.text || 'INCORRECT ANSWER FROM A USER');
+        ctx.session.curStep++;
+        if (ctx.session.curStep < questions.length) {
+            ctx.reply(questions[ctx.session.curStep]);
+        }
+        else {
+            ctx.reply('Thank you for your answers!! Updating the dashboard..');
+            const answers = ctx.session.answers;
+            bot.api.sendMessage(targetChatID, `${answers[0]}, ${answers[1]}, ${answers[2]}`);
+            ctx.session.curStep = 0;
+            ctx.session.answers = [];
+        }
+    }
 })
 
 bot.start();
